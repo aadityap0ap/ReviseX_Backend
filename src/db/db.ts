3d0a308467c092b0db42092f4dcfed04cda9fdf2
aadityap0ap <adityabharti450ap@gmail.com>
2026-07-28 11:13:19 +0530

    Database created and userSchema defined

---
+++ src/db/db.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+export async function ConnectDB() {
+    await mongoose.connect(process.env.MONGO_URL!);
+}
+
+const userSchema = new mongoose.Schema({
+    email : {
+        type : String,
+        unique : true,
+        require : true
+    },
+
+    username : {
+        type : String,
+        unique : true,
+        require : true
+    },
+
+    password : {
+        type : String,
+        unoque : true,
+        require : true
+    }
+})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("hello");
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import { ConnectDB } from "./db/db";
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+async function startServer() {
+    try {
+        await ConnectDB();
+        console.log("Database Connected");
+
+        app.get("/", (req, res) => {
+            res.send("ReviseX Backend is Running");
+        });
+
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error("Failed to start server:", error);
+    }
+}
+
+startServer();
