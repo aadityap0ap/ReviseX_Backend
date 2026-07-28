e7d17f14a16fc3a58276c681292fea8126a47608
aadityap0ap <adityabharti450ap@gmail.com>
2026-07-28 12:54:01 +0530

    Middlewares added

---
+++ src/middlewares/middleware.ts
@@ -0,0 +1,44 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+
+interface JwtPayload {
+    id: string;
+}
+
+export function authMiddleware(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+
+    
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({
+            message: "Authorization header missing"
+        });
+    }
+
+    if (!authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({
+            message: "Invalid authorization format"
+        });
+    }
+
+    try {
+        const token = authHeader.split(" ")[1];
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET!
+        ) as JwtPayload;
+        (req as any).userId = decoded.id;
+        next();
+
+    } catch (error) {
+        return res.status(401).json({
+            message: "Invalid or expired token"
+        });
+    }
+}
