9d5c80e3975aa81da15ee742498fd03aa1fbed59
aadityap0ap <adityabharti450ap@gmail.com>
2026-08-03 18:15:37 +0530

    an endpoint to get all the question solved and added by the user is implmented

--- src/controllers/questionController.ts
+++ src/controllers/questionController.ts
@@ -1,5 +1,5 @@
 import { Request,Response } from "express";
-import { question } from "../db/db";
+import { question, user } from "../db/db";
 
 export const addQuestion  = async(req: Request,res:Response) => {
     try{
@@ -50,4 +50,22 @@ export const addQuestion  = async(req: Request,res:Response) => {
             message : "Internal Server Error and BackeEnd Failure"
         });
     }
+};
+
+export const allQuestions = async(req : Request, res : Response) => {
+    try{
+        const userId = (req as any).userId;
+        const Questions = await question.find({userId});
+        return res.status(200).json({
+            message : "Your Questions are as followed",
+            count : Questions.length,
+            data : Questions
+        })
+    }
+    catch(error){
+        return res.status(500).json({
+            message : "Internal Server Error and Backend Failure!"
+        })
+    }
 }
+
--- src/routes/questionRoutes.ts
+++ src/routes/questionRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/middleware";
-import { addQuestion } from "../controllers/questionController";
+import { addQuestion, allQuestions } from "../controllers/questionController";
 
 const router = express.Router();
 
 router.post("/",authMiddleware,addQuestion);
+router.get("/",authMiddleware,allQuestions);
 export default router;
