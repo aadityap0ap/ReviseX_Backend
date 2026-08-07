3dbb8555a773d0cc45c0f30153dc35a756da2c1f
aadityap0ap <adityabharti450ap@gmail.com>
2026-08-07 16:12:12 +0530

    index.ts updated with regards to revison endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import express from "express";
 import { ConnectDB } from "./db/db";
 import auth from "./routes/authRoutes";
 import profile from "./routes/userRoutes";
-import questions from "./routes/questionRoutes"
+import questions from "./routes/questionRoutes";
+import revise from "./routes/revisionRoutes";
 
 const app = express();
 const PORT = 3000;
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/authRoutes",auth);
 app.use("/userRoutes",profile);
 app.use("/questionRoutes",questions);
+app.use("/reviseQuestions",revise);
 
 async function startServer() {
     try {
