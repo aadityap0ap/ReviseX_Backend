db75a3a899ba86d7b49ff993de7cd0ab70a572e3
aadityap0ap <adityabharti450ap@gmail.com>
2026-08-10 18:16:00 +0530

    authmiddleware added to statsroutes

--- src/routes/statisticsRoutes.ts
+++ src/routes/statisticsRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { getStatistics } from "../controllers/statisticsController";
+import { authMiddleware } from "../middlewares/middleware";
 
 const router = express.Router();
 
-router.get("/",getStatistics);
+router.get("/",authMiddleware,getStatistics);
 
 export default router;
