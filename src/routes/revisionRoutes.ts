7f9e46fd3e9444091e69f03d1747b76babf69cd4
aadityap0ap <adityabharti450ap@gmail.com>
2026-08-10 20:07:12 +0530

    getquestion comtroller integreted in revisionRoutes

--- src/routes/revisionRoutes.ts
+++ src/routes/revisionRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/middleware";
-import { getTodaysRevision, reviseQuestion } from "../controllers/revisonController";
+import { getQuestions, getTodaysRevision, reviseQuestion } from "../controllers/revisonController";
 
 const router = express.Router();
 
 router.patch("/revise/:id",authMiddleware,reviseQuestion);
 router.get("/today",authMiddleware,getTodaysRevision);
+router.get("/search",authMiddleware,getQuestions);
 
 export default router;
